013f0f8d37dc689d9097409976be2c2f6fbc37e1
martin mithi <gathitumithi9000@gmail.com>
2022-12-09 22:25:47 +0300

    commit tree 4c96616428d2373105eab5e65390f03168c1f7d0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+    before_action :authorize
+      skip_before_action :authorize, only: [:create]
+
+      def authorize
+        return render json: { error: "Invalid username or password" }, status: :unauthorized unless session.include? :user_id
+    end
 
 end
